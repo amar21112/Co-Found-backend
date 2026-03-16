1a5ef8b0ff29aac72604e6ba8a9a46850e1dadee
ammar <amarya211fgff@gmail.com>
2026-03-16 17:23:50 +0200

    back to mb4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        Schema::defaultStringLength(1000);
+        Schema::defaultStringLength(191);
     }
 }
